f17fc031805bc42afd4b2bfd5ba6f41c224d604c
massoudTheDude <mahermassoud@gmail.com>
2019-10-09 10:50:00 -0400

    ipython startup script

---
+++ ipythonrc.py
@@ -0,0 +1,17 @@
+# This should be put into ~/.ipython/profile_default/startup
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+
+fig = plt.figure(figsize=(8,8))
+
+def qplot(path="qplot.png", width=8, height=8):
+  path = os.path.join("/Users/massoudmaher/Desktop/", path)
+  plt.savefig(path)
+  plt.figure(figsize=(width,height))
+
+def replot(width=8,height=8):
+  return plt.figure(figsize=(width,height))
